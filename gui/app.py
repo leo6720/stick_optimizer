48324f67eea42cd94754fbb8c3a3bb16c273ac12
Leonardo Chiste <47359385+leo6720@users.noreply.github.com>
2026-08-05 14:12:28 +0000

    edited

--- gui/app.py
+++ gui/app.py
@@ -384,7 +384,7 @@ class OptimizerApp(tk.Tk):
         left_pane = ttk.Frame(main_pane, style="Sidebar.TFrame", padding=12)
         right_pane = ttk.Frame(main_pane, padding=(12, 12, 12, 0))
 
-        main_pane.add(left_pane, weight=8)
+        main_pane.add(left_pane, weight=10)
         main_pane.add(right_pane, weight=25)
 
         right_toolbar = ttk.Frame(right_pane)
